6254a3dffc1c514bafdc1015798bd373668f6872
github username <github email address>
2017-07-19 17:02:07 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,21 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
   end
 
+  # We need to explicitly define a genre= method in order to write to our CONSTANT: GENRES:
+  def genre=(genre)
+  	@genre = genre
+  	# Now that we've assigned a genre to a book, we also want to push that genre to GENRES:
+  	GENRES << genre
+  end
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
